b9f538974d36163d6ec7ba481df7f985d44c5ecc
hudson88@yandex.ru <hudson88@yandex.ru>
2021-07-20 18:38:32 +0300

    add responsive layout for purchase section

--- src/js/partials/data-purchase-slider.js
+++ src/js/partials/data-purchase-slider.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+	const purchaseDotsWrapper = document.querySelector(".data-purchase__dots");
+
+	const purchaseDotsSlider = new Swiper(purchaseDotsWrapper, {
+		slidesPerView: "auto",
+	});
+
 	const purchaseSlider = new Swiper(".data-purchase__slider", {
 		slidesPerView: 1,
 		centeredSlides: true,
@@ -6,7 +12,22 @@ $(document).ready(function () {
 			nextEl: ".swiper-button-next",
 			prevEl: ".swiper-button-prev",
 		},
+		thumbs: {
+			swiper: purchaseDotsSlider,
+		},
 	});
 
-	const purchaseDots = new Swiper(".data-purchase__dots", {});
+	purchaseSlider.on("slideChange", function () {
+		const dotActive = purchaseDotsWrapper.querySelector(".active");
+		const thumbActive = purchaseDotsWrapper.querySelector(
+			".swiper-slide-thumb-active"
+		);
+
+		dotActive.classList.remove("active");
+		thumbActive.classList.add("active");
+	});
+
+	function checkWidth() {
+		const media = window;
+	}
 });
