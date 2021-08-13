bc4632124a9f29fa24cc8ea2be629e6758ec180e
hudson88@yandex.ru <hudson88@yandex.ru>
2021-08-13 17:44:36 +0300

    fix js errors

--- src/js/partials/fields-slider.js
+++ src/js/partials/fields-slider.js
@@ -1,103 +1,105 @@
 $(window).on("load", () => {
-	const carouselEl = document.querySelector(".fields__slider"),
-		showBtns = carouselEl.querySelectorAll('.fields__slide-show');
+  const fieldsCarouselEl = document.querySelector(".fields__slider");
 
-	if (showBtns) {
-		showBtns.forEach(function (el) {
-			el.addEventListener('click', function (e) {
-				let text = el.querySelector('span');
+  if (fieldsCarouselEl) {
+    const showBtns = fieldsCarouselEl.querySelectorAll(".fields__slide-show");
+    showBtns.forEach(function (el) {
+      el.addEventListener("click", function (e) {
+        let text = el.querySelector("span");
 
-				e.preventDefault();
-				el.classList.toggle('rotate');
-				el.previousElementSibling.classList.toggle('show');
-				text.innerText === 'показать' ? text.innerText = 'скрыть' : text.innerText = 'показать';
-			});
-		});
-	}
+        e.preventDefault();
+        el.classList.toggle("rotate");
+        el.previousElementSibling.classList.toggle("show");
+        text.innerText === "показать"
+          ? (text.innerText = "скрыть")
+          : (text.innerText = "показать");
+      });
+    });
+  }
 
-	if (carouselEl) {
-		const businessCasesCarousel = new Swiper(carouselEl, {
-			//loop: true,
-			speed: 300,
-			slidesPerView: "auto",
-			spaceBetween: 32,
-			navigation: {
-				nextEl: ".swiper-button-next",
-				prevEl: ".swiper-button-prev",
-			},
-			pagination: {
-				el: ".swiper-pagination",
-			},
-		});
+  if (fieldsCarouselEl) {
+    const businessCasesCarousel = new Swiper(fieldsCarouselEl, {
+      //loop: true,
+      speed: 300,
+      slidesPerView: "auto",
+      spaceBetween: 32,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
 
-		const filtersSlider = new Swiper(".fields__categories", {
-			slidesPerView: "auto",
-			spaceBetween: 20,
-			breakpoints: {
-				768: {
-					enabled: false,
-				},
-			},
-		});
+    const filtersSlider = new Swiper(".fields__categories", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      breakpoints: {
+        768: {
+          enabled: false,
+        },
+      },
+    });
 
-		const filtersBtns = document.querySelectorAll(
-			".fields__filter[data-target]"
-		);
+    const filtersBtns = document.querySelectorAll(
+      ".fields__filter[data-target]"
+    );
 
-		const slides = document.querySelectorAll(".fields__slide");
-		const filters = document.querySelectorAll(".fields__filter");
+    const slides = document.querySelectorAll(".fields__slide");
+    const filters = document.querySelectorAll(".fields__filter");
 
-		if (filtersBtns && slides) {
-			filterSlides();
+    if (filtersBtns && slides) {
+      filterSlides();
 
-			filtersBtns.forEach((it) => {
-				it.addEventListener("click", function (e) {
-					filtersBtns.forEach((it) => {
-						it.classList.remove("active");
-					});
+      filtersBtns.forEach((it) => {
+        it.addEventListener("click", function (e) {
+          filtersBtns.forEach((it) => {
+            it.classList.remove("active");
+          });
 
-					this.classList.add("active");
-					filterSlides(this);
-				});
-			});
+          this.classList.add("active");
+          filterSlides(this);
+        });
+      });
 
-			filtersSlider.on("activeIndexChange", (event) => {
-				filterSlides(filters[filtersSlider.activeIndex]);
-			});
-		}
+      filtersSlider.on("activeIndexChange", (event) => {
+        filterSlides(filters[filtersSlider.activeIndex]);
+      });
+    }
 
-		function filterSlides(current) {
-			const activeFilter =
-				current || document.querySelector(".fields__filter.active");
-			const target = activeFilter.dataset.target;
+    function filterSlides(current) {
+      const activeFilter =
+        current || document.querySelector(".fields__filter.active");
+      const target = activeFilter.dataset.target;
 
-			if (current) {
-				carouselEl.classList.add("opacity");
+      if (current) {
+        fieldsCarouselEl.classList.add("opacity");
 
-				setTimeout(() => {
-					carouselEl.classList.remove("opacity");
-					for (slide of slides) {
-						let categories = slide.dataset.category.split(",");
+        setTimeout(() => {
+          fieldsCarouselEl.classList.remove("opacity");
+          for (slide of slides) {
+            let categories = slide.dataset.category.split(",");
 
-						categories = categories.map((it) => {
-							return it.trim();
-						});
-						slide.hidden = !categories.includes(target);
-						slide.style.display = slide.hidden ? "none" : "block";
-					}
-					businessCasesCarousel.update();
-				}, 550);
-			} else {
-				for (slide of slides) {
-					let categories = slide.dataset.category.split(",");
-					categories = categories.map((it) => {
-						return it.trim();
-					});
-					slide.hidden = !categories.includes(target);
-					slide.style.display = slide.hidden ? "none" : "block";
-				}
-				businessCasesCarousel.update();
-			}
-		}
-	}
+            categories = categories.map((it) => {
+              return it.trim();
+            });
+            slide.hidden = !categories.includes(target);
+            slide.style.display = slide.hidden ? "none" : "block";
+          }
+          businessCasesCarousel.update();
+        }, 550);
+      } else {
+        for (slide of slides) {
+          let categories = slide.dataset.category.split(",");
+          categories = categories.map((it) => {
+            return it.trim();
+          });
+          slide.hidden = !categories.includes(target);
+          slide.style.display = slide.hidden ? "none" : "block";
+        }
+        businessCasesCarousel.update();
+      }
+    }
+  }
 });
