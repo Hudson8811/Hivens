10df2618c2f88adf2a9c3c955ea707f26a995b0a
hudson88@yandex.ru <hudson88@yandex.ru>
2021-08-16 12:50:48 +0300

    п. 4

--- src/js/partials/fields-slider.js
+++ src/js/partials/fields-slider.js
@@ -1,89 +1,90 @@
 $(window).on("load", () => {
-	const carouselEl = document.querySelector(".fields__slider");
+  const carouselEl = document.querySelector(".fields__slider");
 
-	if (carouselEl) {
-		const businessCasesCarousel = new Swiper(carouselEl, {
-			//loop: true,
-			speed: 300,
-			slidesPerView: "auto",
-			spaceBetween: 32,
-			navigation: {
-				nextEl: ".swiper-button-next",
-				prevEl: ".swiper-button-prev",
-			},
-			pagination: {
-				el: ".swiper-pagination",
-			},
-		});
+  if (carouselEl) {
+    const businessCasesCarousel = new Swiper(carouselEl, {
+      //loop: true,
+      speed: 300,
+      slidesPerView: "auto",
+      spaceBetween: 32,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
 
-		const filtersSlider = new Swiper(".fields__categories", {
-			slidesPerView: "auto",
-			spaceBetween: 20,
-			breakpoints: {
-				768: {
-					enabled: false,
-				},
-			},
-		});
+    const filtersSlider = new Swiper(".fields__categories", {
+      slidesPerView: 1,
+      spaceBetween: 0,
+      slideToClickedSlide: true,
+      breakpoints: {
+        768: {
+          enabled: false,
+        },
+      },
+    });
 
-		const filtersBtns = document.querySelectorAll(
-			".fields__filter[data-target]"
-		);
+    const filtersBtns = document.querySelectorAll(
+      ".fields__filter[data-target]"
+    );
 
-		const slides = document.querySelectorAll(".fields__slide");
-		const filters = document.querySelectorAll(".fields__filter");
+    const slides = document.querySelectorAll(".fields__slide");
+    const filters = document.querySelectorAll(".fields__filter");
 
-		if (filtersBtns && slides) {
-			filterSlides();
+    if (filtersBtns && slides) {
+      filterSlides();
 
-			filtersBtns.forEach((it) => {
-				it.addEventListener("click", function (e) {
-					filtersBtns.forEach((it) => {
-						it.classList.remove("active");
-					});
+      filtersBtns.forEach((it, index) => {
+        it.addEventListener("click", function (e) {
+          filtersBtns.forEach((it) => {
+            it.classList.remove("active");
+          });
 
-					this.classList.add("active");
-					filterSlides(this);
-				});
-			});
+          this.classList.add("active");
+          filterSlides(this);
+        });
+      });
 
-			filtersSlider.on("activeIndexChange", (event) => {
-				filterSlides(filters[filtersSlider.activeIndex]);
-			});
-		}
+      filtersSlider.on("activeIndexChange", (event) => {
+        filterSlides(filters[filtersSlider.activeIndex]);
+      });
+    }
 
-		function filterSlides(current) {
-			const activeFilter =
-				current || document.querySelector(".fields__filter.active");
-			const target = activeFilter.dataset.target;
+    function filterSlides(current) {
+      const activeFilter =
+        current || document.querySelector(".fields__filter.active");
+      const target = activeFilter.dataset.target;
 
-			if (current) {
-				carouselEl.classList.add("opacity");
+      if (current) {
+        carouselEl.classList.add("opacity");
 
-				setTimeout(() => {
-					carouselEl.classList.remove("opacity");
-					for (slide of slides) {
-						let categories = slide.dataset.category.split(",");
+        setTimeout(() => {
+          carouselEl.classList.remove("opacity");
+          for (slide of slides) {
+            let categories = slide.dataset.category.split(",");
 
-						categories = categories.map((it) => {
-							return it.trim();
-						});
-						slide.hidden = !categories.includes(target);
-						slide.style.display = slide.hidden ? "none" : "block";
-					}
-					businessCasesCarousel.update();
-				}, 550);
-			} else {
-				for (slide of slides) {
-					let categories = slide.dataset.category.split(",");
-					categories = categories.map((it) => {
-						return it.trim();
-					});
-					slide.hidden = !categories.includes(target);
-					slide.style.display = slide.hidden ? "none" : "block";
-				}
-				businessCasesCarousel.update();
-			}
-		}
-	}
+            categories = categories.map((it) => {
+              return it.trim();
+            });
+            slide.hidden = !categories.includes(target);
+            slide.style.display = slide.hidden ? "none" : "block";
+          }
+          businessCasesCarousel.update();
+        }, 550);
+      } else {
+        for (slide of slides) {
+          let categories = slide.dataset.category.split(",");
+          categories = categories.map((it) => {
+            return it.trim();
+          });
+          slide.hidden = !categories.includes(target);
+          slide.style.display = slide.hidden ? "none" : "block";
+        }
+        businessCasesCarousel.update();
+      }
+    }
+  }
 });
